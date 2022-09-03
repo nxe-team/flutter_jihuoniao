c6051b3eee62531043048c7c0d98c4ccb02f0281
anandzhang <hi@anandzhang.com>
2022-09-03 15:03:04 +0800

    feat: 更新 example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_jihuoniao/flutter_jihuoniao.dart';
 import 'package:flutter_jihuoniao_example/config/ad_config.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -26,40 +26,38 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter JiHuoNiao Example'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  FlutterJihuoniao.showSplashAd(slotId: AdConfig.splashId);
-                },
-                child: const Text('开屏广告'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  FlutterJihuoniao.showSplashAd(
-                    slotId: AdConfig.splashId,
-                    logo: 'SplashBottomLogo',
-                  );
-                },
-                child: const Text('开屏广告（带Logo）'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  FlutterJihuoniao.showInterstitialAd(
-                    context: context,
-                    slotId: AdConfig.interstitialId,
-                  );
-                },
-                child: const Text('插屏广告'),
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter JiHuoNiao Example'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                FlutterJihuoniao.showSplashAd(slotId: AdConfig.splashId);
+              },
+              child: const Text('开屏广告'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FlutterJihuoniao.showSplashAd(
+                  slotId: AdConfig.splashId,
+                  logo: 'SplashBottomLogo',
+                );
+              },
+              child: const Text('开屏广告（带Logo）'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                FlutterJihuoniao.showInterstitialAd(
+                  context: context,
+                  slotId: AdConfig.interstitialId,
+                );
+              },
+              child: const Text('插屏广告'),
+            )
+          ],
         ),
       ),
     );
